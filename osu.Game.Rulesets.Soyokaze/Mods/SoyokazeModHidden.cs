3b35c096dd339252908a2bdfdaaf876844ef58e7
nahkd123 <nahkd123@gmail.com>
2021-11-07 09:42:02 +0700

    Add fading approach circles and some options

--- osu.Game.Rulesets.Soyokaze/Mods/SoyokazeModHidden.cs
+++ osu.Game.Rulesets.Soyokaze/Mods/SoyokazeModHidden.cs
@@ -2,8 +2,10 @@
 // See the LICENSE file in the repository root for full licence text.
 
 using System.Linq;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Game.Beatmaps;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Soyokaze.Objects;
@@ -13,6 +15,19 @@ namespace osu.Game.Rulesets.Soyokaze.Mods
 {
     public class SoyokazeModHidden : ModHidden
     {
+        [SettingSource("Fading Approach Circle", "Allow approach circles to fade instead of disappearing")]
+        public Bindable<bool> FadingApproachCircle { get; } = new BindableBool(false);
+
+        [SettingSource("Fading Approach Circle Speed", "Adjust the fading speed of approach circles")]
+        public Bindable<double> FadingApproachCircleSpeed { get; } = new BindableDouble
+        {
+            Precision = 0.05f,
+            MinValue = 0.5,
+            MaxValue = 2.0,
+            Default = 1.0,
+            Value = 1.0
+        };
+
         public override double ScoreMultiplier => 1.09;
         public override string Description => "IT'S UNREADABLE.";
 
@@ -65,9 +80,13 @@ namespace osu.Game.Rulesets.Soyokaze.Mods
                 case DrawableHitCircle circle:
                     fadeTarget = circle.HitCircle;
                     if (!increaseVisibility)
-                        using (circle.BeginAbsoluteSequence(hitObject.StartTime - hitObject.Preempt))
+                        // using (circle.BeginAbsoluteSequence(hitObject.StartTime - hitObject.Preempt))
+                        using (circle.BeginAbsoluteSequence(FadingApproachCircle.Value? fadeOutStartTime : hitObject.StartTime - hitObject.Preempt))
                         {
-                            circle.ApproachCircle.Hide();
+                            if (FadingApproachCircle.Value)
+                                circle.ApproachCircle.FadeOut(fadeOutDuration / FadingApproachCircleSpeed.Value);
+                            else
+                                circle.ApproachCircle.Hide();
                         }
                     goto default;
                 default:
