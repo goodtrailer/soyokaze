07148a3d5bd79fc7f348c0e1429d369092db3e11
Alden Wu <aldenwu0@gmail.com>
2025-04-01 10:46:10 -0700

    Skinning: Use separate `soyokaze/` folder for skin PNGs

---
+++ osu.Game.Rulesets.Soyokaze/Skinning/Defaults/AbstractDefaultComponent.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Alden Wu <aldenwu0@gmail.com>. Licensed under the MIT Licence.
+// See the LICENSE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+
+namespace osu.Game.Rulesets.Soyokaze.Skinning.Defaults
+{
+    public abstract partial class AbstractDefaultComponent : CompositeDrawable
+    {
+        public virtual Texture Texture { get; init; }
+
+        public AbstractDefaultComponent()
+        {
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+        }
+
+        protected abstract SoyokazeSkinComponents Component { get; }
+
+        [BackgroundDependencyLoader]
+        private void load(TextureStore textures)
+        {
+            var lookup = new SoyokazeSkinComponentLookup(Component);
+            AddInternal(new Sprite
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Texture = Texture ?? textures.Get(lookup.StoreName),
+            });
+        }
+    }
+}
--- osu.Game.Rulesets.Soyokaze/Skinning/Defaults/DefaultApproachCircle.cs
+++ osu.Game.Rulesets.Soyokaze/Skinning/Defaults/DefaultApproachCircle.cs
@@ -1,33 +1,10 @@
 ﻿// Copyright (c) Alden Wu <aldenwu0@gmail.com>. Licensed under the MIT Licence.
 // See the LICENSE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.Textures;
-using osu.Game.Skinning;
-
 namespace osu.Game.Rulesets.Soyokaze.Skinning.Defaults
 {
-    public partial class DefaultApproachCircle : CompositeDrawable
+    public partial class DefaultApproachCircle : AbstractDefaultComponent
     {
-        public DefaultApproachCircle()
-        {
-            Anchor = Anchor.Centre;
-            Origin = Anchor.Centre;
-        }
-
-        [BackgroundDependencyLoader]
-        private void load(TextureStore textures, ISkinSource skin)
-        {
-            var lookup = new SoyokazeSkinComponentLookup(SoyokazeSkinComponents.ApproachCircle);
-            AddInternal(new Sprite
-            {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Texture = skin.GetTexture(lookup.LookupName) ?? textures.Get(lookup.StoreName),
-            });
-        }
+        protected override SoyokazeSkinComponents Component => SoyokazeSkinComponents.ApproachCircle;
     }
 }
